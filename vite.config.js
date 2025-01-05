e6094eed40f9ff692d569985fd4e53d432daca90
Sandro Mego <zsmr11@gmail.com>
2025-01-05 15:24:34 -0500

    sin git hub pages npm

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "san2008-prog/react-task-example",
+  base: "react-task-example",
 });
